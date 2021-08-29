cd431fd1de01b80e0584ff18a85e656b7d68997e
Alexey Mozhenin <dvorkin83@gmail.com>
2021-08-29 21:48:35 +0600

    Stub E

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("25 0 25 0");
+        System.out.println("125 0 125 0");
+        System.out.println("125 100 25 0");
+        System.out.println("125 100 25 0");
+        System.out.println("125 110 15 0");
+        System.out.println("125 120 5 0");
+        System.out.println("125 130 0 5");
+        System.out.println("0 0 0 0");
     }
 }
