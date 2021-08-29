29685dfd0ddd168a24b1459766290907903e00df
Alexey Mozhenin <dvorkin83@gmail.com>
2021-08-29 18:35:45 +0600

    First attempt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        int n = scan.nextInt();
+        int l = scan.nextInt();
+
+        int div = l / n;
+        int rem = l % n;
+        if (rem == 0) {
+            System.out.println(div);
+        } else {
+            System.out.println(div + 1);
+        }
     }
 }
