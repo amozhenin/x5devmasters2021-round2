b6f2f65ea3645e7c0227bcbf9036f0110ff581fc
Alexey Mozhenin <dvorkin83@gmail.com>
2021-08-30 16:00:30 +0600

    Stub for C

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("1 1 2 3");
+        System.out.println("2 1 2 3");
+        System.out.println("3 1 2 3");
+        System.out.println("4 1 2 3");
+        System.out.println("5 4 5");
+        System.out.println("6 4 5");
     }
 }
